f61f8690f3b61494acc91c762672a58222274bf5
damonmaldonado <damon@mpwmarketing.com>
2016-05-24 17:02:39 -0400

    Update heron_creek_additions.php

--- heron_creek_additions.php
+++ heron_creek_additions.php
@@ -7,20 +7,20 @@ Author: dmm
 Text Domain: heron_creek
 */
 function featured_box_item ( $atts, $content = null) {
-      $atts = shortcode_atts( array(
+      $atta = shortcode_atts( array(
         'background-image' => '',
         'title_text' => '',
         'button_text' => '',
     ), $atts );
     $cont = '';
     $cont .= '<div class="conv-square"';
-    if ($atts['background-image'] !== '') {
-      $cont .= ' style="background-image: url(\''.$atts['background-image'].'\');"';
+    if ($atta['background-image'] !== '') {
+      $cont .= ' style="background-image: url(\''.$atta['background-image'].'\');"';
     }
     $cont .= '>';
-    $cont .= '<div class="box-title">'.$atts['title_text'].'</div>';
+    $cont .= '<div class="box-title">'.$atta['title_text'].'</div>';
     $cont .= '<div class="box-content">'.$content.'</div>';
-    if ($atts['button_text'] !== ''){$cont .= '<div class="box-button">'.$atts['button_text'].'</div>';}
+    if ($atta['button_text'] !== ''){$cont .= '<div class="box-button">'.$atta['button_text'].'</div>';}
     $cont .= '</div>';
     
     return do_shortcodes($cont);

437c1f63d8c97dd371493355404cc7996ba8cf4c
damonmaldonado <damon@mpwmarketing.com>
2016-05-24 16:49:56 -0400

    Initial plugin

--- heron_creek_additions.php
+++ heron_creek_additions.php
@@ -6,3 +6,24 @@ Version: 0.1.0
 Author: dmm
 Text Domain: heron_creek
 */
+function featured_box_item ( $atts, $content = null) {
+      $atts = shortcode_atts( array(
+        'background-image' => '',
+        'title_text' => '',
+        'button_text' => '',
+    ), $atts );
+    $cont = '';
+    $cont .= '<div class="conv-square"';
+    if ($atts['background-image'] !== '') {
+      $cont .= ' style="background-image: url(\''.$atts['background-image'].'\');"';
+    }
+    $cont .= '>';
+    $cont .= '<div class="box-title">'.$atts['title_text'].'</div>';
+    $cont .= '<div class="box-content">'.$content.'</div>';
+    if ($atts['button_text'] !== ''){$cont .= '<div class="box-button">'.$atts['button_text'].'</div>';}
+    $cont .= '</div>';
+    
+    return do_shortcodes($cont);
+    
+}
+add_shortcode('featured_box', 'featured_box_item');

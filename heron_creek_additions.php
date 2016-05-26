0fbc537da7b5caaf4d94f982ef96a02e22fd60e0
Damon Maldonado <damon@mpwmarketing.com>
2016-05-26 10:18:56 -0400

    fixes to script registers

--- heron_creek_additions.php
+++ heron_creek_additions.php
@@ -2,16 +2,16 @@
 /*
 Plugin Name: Heron Creek Additions
 Description: Additional Functions For Site
-Version: 0.1.0
+Version: 0.1.1
 Author: dmm
 Text Domain: heron_creek
 */
 function heron_creek_scripts_method() {
     wp_enqueue_style( 'conversion-boxes', plugins_url( '/css/conversion-boxes.css' , __FILE__ ) );
     wp_enqueue_style( 'scrollpane-css', plugins_url( '/css/jquery.jscrollpane.css' , __FILE__ ) );
-    wp_register_script( 'jscrollpane', plugins_url( '/js/jquery.jscrollpane.js',array ('jquery'), false, true); );
-    wp_register_script( 'jqmousewheel', plugins_url( '/js/jquery.mousewheel.js',array ('jquery'), false, true); );
-    wp_register_script( 'mwheelIntent', plugins_url( '/js/mwheelIntent.js',array ('jquery'), false, true); );
+    wp_register_script( 'jscrollpane', plugins_url( '/js/jquery.jscrollpane.js', __FILE__ ) ,array ('jquery'), false, true );
+    wp_register_script( 'jqmousewheel', plugins_url( '/js/jquery.mousewheel.js', __FILE__ ),array ('jquery'), false, true );
+    wp_register_script( 'mwheelIntent', plugins_url( '/js/mwheelIntent.js', __FILE__ ),array ('jquery'), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'heron_creek_scripts_method' );
 
